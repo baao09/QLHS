595e35775a35a7178de6731b66ef93515977d40c
baao09 <baothai80004@gmail.com>
2026-03-08 17:06:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 8800;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 
 app.use(express.json());
 app.use(express.static("public"));
